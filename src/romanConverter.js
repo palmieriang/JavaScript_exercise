cc1edc1341af5a7d7a43f0ebab5e330bb3f96551
Angelo Palmieri <palmieri.ang@gmail.com>
2016-11-11 12:23:37 +0000

    roman numbers converter

---
+++ src/romanConverter.js
@@ -0,0 +1,14 @@
+function convertToRoman(num) {
+  var roman = ""; 
+  var romanNumeral = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
+  var numbers = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  for (var i=0; i<numbers.length; i++) {
+     while(num>=numbers[i]) {
+       roman = roman + romanNumeral[i];
+       num = num - numbers[i];
+     }
+  }
+ return roman;
+}
+
+convertToRoman(36);
