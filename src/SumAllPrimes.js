cc5d2e7e943f70609a9cc20ebcd90e9fea66e021
Angelo Palmieri <palmieri.ang@gmail.com>
2016-11-15 10:22:43 +0000

    Sum all primes number

---
+++ src/SumAllPrimes.js
@@ -0,0 +1,28 @@
+// Sum All Primes
+
+function sumPrimes(num) {
+  
+  function isPrime(number) {
+    for(var i=2; i<=number; i++) {
+      if(number % i === 0 && number != i) {
+        return false;
+      }
+    }
+    return true;
+  }
+  
+  if(num===1) {
+    return 0;
+  }
+  
+  if(isPrime(num) === false) {
+    return sumPrimes(num - 1);
+  }
+  
+  if(isPrime(num) === true) {
+    return num + sumPrimes(num -1);
+  }
+  
+}
+
+sumPrimes(10);
