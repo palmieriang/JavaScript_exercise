981f53d75345baaafb7ce5afd8a2a2657600ae31
Angelo Palmieri <palmieri.ang@gmail.com>
2016-11-17 16:42:43 +0000

    Every Thing Be True

---
+++ src/everythingBeTrue.js
@@ -0,0 +1,32 @@
+//Everything Be True 
+
+function truthCheck(collection, pre) {
+  // Create a counter to check how many are true.
+  var counter = 0;
+  // Check for each object
+  for (var c in collection) {
+    // If it is has property and value is truthy
+    if (collection[c].hasOwnProperty(pre) && Boolean(collection[c][pre])) {
+      counter++;
+    }
+  }
+  // Outside the loop, check to see if we got true for all of them and return true or false
+  return counter == collection.length;
+}
+
+truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex");
+
+
+/*function truthCheck(collection, pre) {
+  // Is everyone being true?
+ 
+  if(collection.hasOwnProperty(pre)) {
+     return true;
+     } else {
+       return false;
+     }
+
+}
+
+truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex");
+*/
